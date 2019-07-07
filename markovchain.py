fa67a7e8620f0ee6ebc50725d7de650929d47c69
Bhagam Chandan Chowdary <chandanchoudary@hotmail.com>
2019-07-07 16:17:35 +0530

    formatted the code

--- markovchain.py
+++ markovchain.py
@@ -16,8 +16,10 @@ dictionary = []
 
 for text in text_array:
 	line_array = text.split(' ')
+
 	for index, word in enumerate(line_array):
 		word_data = {'name': '', 'start': {'status': False, 'count': 0}, 'next': [], 'end': False,}
+
 		if not any(item['name'] == word for item in dictionary):
 			word_data['name'] = word
 			dictionary.append(word_data)
@@ -25,17 +27,21 @@ for text in text_array:
 			for item in dictionary:
 				if item['name'] == word:
 					word_data = item
-					break;
+					break
+
 		if index == 0:
 			dictionary[dictionary.index(word_data)]['start'] = {'status': True, 'count': word_data['start']['count']+1}
+
 		if index != len(line_array)-1:
+
 			if not any(item['word'] == line_array[index+1] for item in dictionary[dictionary.index(word_data)]['next']):
 				dictionary[dictionary.index(word_data)]['next'].append({'word': line_array[index+1], 'count': 1})
 			else:
 				for ind, item in enumerate(dictionary[dictionary.index(word_data)]['next']):
 					if item['word'] == line_array[index+1]:
 						dictionary[dictionary.index(word_data)]['next'][ind]['count'] += 1;
-						break;
+						break
+
 		else:
 			dictionary[dictionary.index(word_data)]['end'] = True
 
@@ -46,7 +52,9 @@ option = input("Generate a Sentence(1), exit(any other key):")
 start_words = []
 		
 for word in dictionary:
+
 	if word['start']['status']:
+
 		for instance in range(0, word['start']['count']):
 			start_words.append(word['name'])
 
@@ -61,18 +69,22 @@ while option == '1':
 	while word_index < len(dictionary):
 		item = dictionary[word_index]
 		word_data = {}
+
 		if item['name'] == word:
 			word_data = item
 			next_words = []
+
 			if word_data['end']:
 				print(word_data)
 				sentence = sentence+'.'
 				break
 			else:
-				#print(word_data)
+
 				for next_word in word_data['next']:
+
 					for instance in range(0, next_word['count']):
 						next_words.append(next_word['word'])
+
 				add_word = random.choice(next_words)
 				sentence = sentence + ' ' + add_word
 				word = add_word
